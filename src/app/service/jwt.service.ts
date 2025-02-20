fadf1266e8c700180bbb89dc6cf3c21ad8f7d273
hansajasandeepabadalge <hansajasandeepabadalge@gmail.com>
2025-02-20 17:13:26 +0530

    first commit

---
+++ src/app/service/jwt.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const BASE_URL = 'http://localhost:8080/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JwtService {
+
+  constructor(private http: HttpClient) {
+
+  }
+  register(signRequest:any): Observable <any> {
+    return this.http.post(BASE_URL +  'signup' , signRequest);
+  }
+
+}
